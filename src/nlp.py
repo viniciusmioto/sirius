9ebfa00fe9bd1d43da7f9dd96f56f2357f3d7e12
Vinicius Mioto <vinemioto@gmail.com>
2021-04-10 18:24:13 -0300

    create bag of words
    labels, phrases and words

--- src/nlp.py
+++ src/nlp.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy as numpy
+import numpy as np
 import tflearn
 import tensorflow as tf
 import random
@@ -13,13 +13,44 @@ with open("../data/intents.json") as file:
 
 phrases = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data['intents']:
     for pattern in intent['patterns']:
         words = nltk.word_tokenize(pattern)
         phrases.extend(words)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent['tag'])
 
         if intent['tag'] not in labels:
             labels.append(intent['tag'])
+
+phrases = [stemmer.stem(w.lower()) for w in phrases]
+phrases = sorted(list(set(phrases)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+    words = [stemmer.stem(w) for w in doc]
+
+    for w in phrases:
+        if w in words:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = np.array(training)
+output = np.array(output)
